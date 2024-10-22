2db83f2653bdd20f5119a15893e9d9e2e4d71c45
Matus <matuskocik@gmail.com>
2024-10-22 12:06:23 +0200

    created model Income and Expense

--- backend/finance/models.py
+++ backend/finance/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# starting models
+
+class Income(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, help_text="Income Title")
+    amount = models.FloatField(help_text="Income Amount")
+    description = models.TextField(help_text="Income Text", blank=True, default="")
+    created_at = models.DateField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        verbose_name: str = "Income"
+        verbose_name_plural: str = "Incomes"
+        ordering: list[str] = ["-created_at"]
+
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50, help_text="Expense Title")
+    amount = models.FloatField(help_text="Expense Amount")
+    description = models.TextField(help_text="Expense Description", blank=True, default="")
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name: str = "Expense"
+        verbose_name_plural: str = "Expenses"
+        ordering: list[str] = ["-created_at"]
